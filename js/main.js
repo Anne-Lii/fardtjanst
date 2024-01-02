8019547e1028c08325377a8c780e1610c9549f20
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-01-02 10:31:42 +0100

    lagt till ikoner på knappar, lagt till bekräftelsesidan och gjort den

--- js/main.js
+++ js/main.js
@@ -11,14 +11,39 @@ closeButtonEl.addEventListener("click", togglemenu);
 function togglemenu() {
 
     let navMenuEl = document.getElementById("nav-menu");
-    //console.log("det funkar");
+    
 
     let style = window.getComputedStyle(navMenuEl);
-    console.log(style.display);
+    
 
     if(style.display === "none") {
         navMenuEl.style.display = "block";
     } else {
         navMenuEl.style.display = "none";
+    }
+}
+
+
+
+//hämta element från HTML-koden
+let check_återkommandeEl = document.getElementById("check_återkommande");
+
+
+//event
+check_återkommandeEl.addEventListener("click", toggledays);
+
+
+function toggledays() {
+
+    let återkommande_dropmenyEl = document.getElementById("återkommande_dropmeny");
+    console.log("det funkar");
+
+    let styled = window.getComputedStyle(återkommande_dropmenyEl);
+    console.log(styled.display);
+
+    if(styled.display === "none") {
+        återkommande_dropmenyEl.style.display = "block";
+    } else {
+        återkommande_dropmenyEl.style.display = "none";
     }
 }
